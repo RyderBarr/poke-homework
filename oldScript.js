96b3d23b3bd5bc0aba99be8e33d2d3db764850b4
ryder <tootwo2to22@gmail.com>
2025-04-25 18:20:38 -0700

    day 2

---
+++ oldScript.js
@@ -0,0 +1,144 @@
+// old version V
+
+$(document).ready(function(){
+    let pokedex = fetch('https://pokeapi.co/api/v2/pokemon?limit=156&offset=493')
+
+    .then
+    (
+        
+
+        response =>
+
+        {
+
+
+            if(
+                
+
+                !response.ok
+                
+
+                )
+
+                {
+                    
+
+                    throw new Error
+                    (
+                
+                        
+                        'Network response was bad: '
+                        , 
+                        response.statusText
+                        
+
+                    )
+                    
+
+                }
+
+            return response.json
+            (
+
+
+
+            )
+
+
+        }
+
+
+    ).then
+    (
+
+
+        result=>
+        {
+
+
+            for( x of result.results)
+            {
+                let pokemon = fetch(x.url)
+
+                .then
+                (
+                    
+
+                    response =>
+
+                    {
+
+
+                        if(
+                            
+
+                            !response.ok
+                            
+
+                            )
+
+                            {
+                                
+
+                                throw new Error
+                                (
+                            
+                                    
+                                    'Network response was bad: '
+                                    , 
+                                    response.statusText
+                                    
+
+                                )
+                                
+
+                            }
+
+                        return response.json
+                        (
+
+
+
+                        )
+
+
+                    }
+
+
+                ).then(
+
+                    data=>
+                    {
+                        $('body').append(`
+                        <div class='${data.types[0]?.type.name}'>
+        
+                            <h2> ${data.name} </h2>
+
+                            <img src="${data.sprites.front_default}" alt="">
+
+                            <p>base experience: ${data.base_experience}</p>
+                            
+                            <p>type: ${data.types[0]?.type.name}</p>
+                            
+                            <p>ability: ${data.abilities[0]?.ability.name}</p>
+
+                            
+
+                        </div>
+                        `)                    
+
+                    }
+
+
+                )
+
+
+            }
+
+
+        }
+        
+
+    )
+
+})
--- script.js
+++ script.js
@@ -1,131 +1,44 @@
 $(document).ready(function(){
-    let pokedex = fetch('https://pokeapi.co/api/v2/pokemon?limit=156&offset=493')
 
-    .then
-    (
-        
+    function addPokemon(url){
 
-        response =>
+        let pokemon = fetch(url)
+    
+        .then
+        (
+            
 
-        {
+            response =>
+
+            {
 
 
-            if(
-                
-
-                !response.ok
-                
-
-                )
-
-                {
+                if(
                     
 
-                    throw new Error
-                    (
-                
-                        
-                        'Network response was bad: '
-                        , 
-                        response.statusText
-                        
+                    !response.ok
+                    
 
                     )
-                    
-
-                }
-
-            return response.json
-            (
-
-
-
-            )
-
-
-        }
-
-
-    ).then
-    (
-
-
-        result=>
-        {
-
-
-            for( x of result.results)
-            {
-                let pokemon = fetch(x.url)
-
-                .then
-                (
-                    
-
-                    response =>
 
                     {
+                        
 
-
-                        if(
-                            
-
-                            !response.ok
-                            
-
-                            )
-
-                            {
-                                
-
-                                throw new Error
-                                (
-                            
-                                    
-                                    'Network response was bad: '
-                                    , 
-                                    response.statusText
-                                    
-
-                                )
-                                
-
-                            }
-
-                        return response.json
+                        throw new Error
                         (
-
-
+                    
+                            
+                            'Network response was bad: ', response.statusText
+                            
 
                         )
-
-
-                    }
-
-
-                ).then(
-
-                    data=>
-                    {
-                        $('body').append(`
-                        <div class='${data.types[0]?.type.name}'>
-        
-                            <h2> ${data.name} </h2>
-
-                            <img src="${data.sprites.front_default}" alt="">
-
-                            <p>base experience: ${data.base_experience}</p>
-                            
-                            <p>type: ${data.types[0]?.type.name}</p>
-                            
-                            <p>ability: ${data.abilities[0]?.ability.name}</p>
-
-                            
-
-                        </div>
-                        `)                    
+                        
 
                     }
+
+                return response.json
+                (
+
 
 
                 )
@@ -134,9 +47,64 @@ $(document).ready(function(){
             }
 
 
-        }
-        
+        ).then(
 
-    )
+            data=>
+            {
+                $('body').append(`
+                <div class='${data.types[0]?.type.name} ${data.name}'>
+
+                    <h2> ${data.name} </h2>
+
+                    <img src="${data.sprites.front_default}" alt="">
+
+                    <p>base experience: ${data.base_experience}</p>
+                    
+                    <p>type: ${data.types[0]?.type.name}</p>
+                    
+                    <p>ability: ${data.abilities[0]?.ability.name}</p>
+
+                    
+
+                </div>
+                `)                    
+
+            }
+
+
+        )
+
+    }
+
+    // add drillbur
+    addPokemon('https://pokeapi.co/api/v2/pokemon/529/')
+    
+    // add emboar
+    addPokemon("https://pokeapi.co/api/v2/pokemon/500/")
+
+    // add lugia
+    addPokemon("https://pokeapi.co/api/v2/pokemon/249/")
+
+    // add spiritomb
+    addPokemon("https://pokeapi.co/api/v2/pokemon/442/")
+
+    // add gible
+    addPokemon("https://pokeapi.co/api/v2/pokemon/443/")
+
+    // add trubbish
+    addPokemon("https://pokeapi.co/api/v2/pokemon/568/")
+
+    // add krookodile
+    addPokemon("https://pokeapi.co/api/v2/pokemon/553/")
+
+    // add darkrai
+    addPokemon("https://pokeapi.co/api/v2/pokemon/491/")
+
+    // add mamoswine
+    addPokemon("https://pokeapi.co/api/v2/pokemon/473/")
+
+    // add joltik
+    addPokemon("https://pokeapi.co/api/v2/pokemon/595/")
 
 })
+
